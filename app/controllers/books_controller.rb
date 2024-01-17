3d1065d2150efb942f408c62c9c56c1f952659f9
Kohei-70 <152633774+Kohei-70@users.noreply.github.com>
2024-01-17 10:10:38 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,51 @@
 class BooksController < ApplicationController
-  def new
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+    # 4. トップ画面へリダイレクト
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
